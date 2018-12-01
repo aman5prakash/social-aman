8e65be982350d0dff3c42f8838d032481ad846dd
Nishant Jain <nishantkumarajain@gmail.com>
2018-12-01 20:14:05 +0530

    Updated the dockerignore, docker-compose.yml and rabbimq-consumer

--- Models/TopicFromRabbitMq.cs
+++ Models/TopicFromRabbitMq.cs
@@ -15,23 +15,22 @@ namespace SocialServer.Consumers
         public void GetTopicsFromRabbitMQ()
         {
             var factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672, UserName = "rabbitmq", Password = "rabbitmq" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            
+            channel.QueueDeclare(queue: "Topic", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
             {
-                channel.QueueDeclare(queue: "Topic", durable: false, exclusive: false, autoDelete: false, arguments: null);
-                
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-                };
-                channel.BasicConsume(queue: "Topic", autoAck: true, consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "Topic", autoAck: true, consumer: consumer);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     
     }
