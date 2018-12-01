699755fd8a3d21f17d255a9aff54cf592654187f
Prateek Sahay <prateeksahay96@gmail.com>
2018-12-01 17:57:16 +0530

    Injecting Topic Consumer Service in Configure Method

--- Startup.cs
+++ Startup.cs
@@ -65,7 +65,7 @@ namespace backEnd
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, TopicConsumer c)
         {
             if (env.IsDevelopment())
             {
